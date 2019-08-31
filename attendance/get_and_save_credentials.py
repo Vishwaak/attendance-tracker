e2714f046b9fc28274c3f061158c77900d0bd3f6
harshithpabbati <pabbatiharshith@gmail.com>
2019-09-01 00:17:02 +0530

    get_save_credentials: Rename the file

--- attendance/get_and_save_credentials.py
+++ attendance/get_and_save_credentials.py
@@ -4,15 +4,15 @@ import sys
 import requests
 
 
-def get_and_save_auth_token():
-    print("Enter your amFOSS website username and password.")
-    username = input("username: ")
+def get_and_save_credentials():
+    print("Enter your amFOSS CMS Username and Password.")
+    username = input("Username: ")
     password = getpass.getpass("Password: ")
     data = {"username": username, "password": password}
     #Saves username and password
     with open('.credentials', 'w') as file:
         json.dump(data, file)
-        print("Token successfully saved, please run this script whenever you change your credentials.")
+        print("Successfully saved, please run this script whenever you change your credentials.")
 
 if __name__ == '__main__':
-    get_and_save_auth_token()
+    get_and_save_credentials()
